cdebbe72643d4ca67f049073bedbdec7b6cfdd8a
anitta-keyvalue <anitta@keyvalue.systems>
2025-05-15 10:14:37 +0530

    feat: Updated export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { MultiLevelTable } from './components/MultiLevelTable';
 export type { MultiLevelTableProps } from './components/MultiLevelTable'; 
+export type { DataItem } from './types/types';
+export type { Column } from './types/types';
+export type { TableStateWithPagination } from './types/types';
+export type { TableInstanceWithHooks } from './types/types';
+export type { SelectionState } from './types/types';
+
